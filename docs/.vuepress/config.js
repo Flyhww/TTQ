00b3f04a752b3d72d521a11216a8bfe5ad880a4b
lihongwen <lihonmg@shuame.com>
2022-07-14 17:05:21 +0800

    add:  search

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,17 @@
+const { defaultTheme } = require('@vuepress/theme-default')
+const { searchPlugin } = require('@vuepress/plugin-search')
 module.exports = {
+  lang: 'zh-CN',
   title: 'Hello 甜甜圈~',
-  description: 'Just playing around',
+  description: '这是我的第一个 VuePress 站点',
   themeConfig: {
-    sidebar: 'auto',
-    sidebarDepth: 2,
-    displayAllHeaders: true
-  }
+  },
+  theme: defaultTheme({
+    // 在这里进行配置
+  }),
+  plugins: [
+    searchPlugin({
+      // 配置项
+    }),
+  ],
 }
