c576032b2c41f797219fed675d21ea30939a44ed
lihongwen <lihonmg@shuame.com>
2022-07-15 17:02:23 +0800

    feat: 格式更新

--- docs1/.vuepress/config.js
+++ docs1/.vuepress/config.js
@@ -3,8 +3,8 @@ const { searchPlugin } = require('@vuepress/plugin-search')
 const path = require('path')
 
 module.exports = {
-  base:"/TTQ/",
-  dest:path.resolve(__dirname,'../../docs'),
+  base: "/TTQ/",
+  dest: path.resolve(__dirname,'../../docs'),
   lang: 'zh-CN',
   title: 'Hello 甜甜圈~',
   description: '这是我的第一个 VuePress 站点',
